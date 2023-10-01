c4eab88f14ae704fc016fe57eb2972882461e346
09014230 <123412134+09014230@users.noreply.github.com>
2023-10-01 12:43:39 -0400

    Using FindObjectOfType

--- Snow-Boarder/Assets/Scripts/PlayerController.cs
+++ Snow-Boarder/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,11 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] float ftTorqueAmount = 1f;
+    [SerializeField] float ftBoostSpeed = 30f;
+    [SerializeField] float ftBaseSpeed = 20f;
+
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +20,28 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = ftBoostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = ftBaseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(ftTorqueAmount);
+            surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
